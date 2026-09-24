77521d534a1ca506576061b1c30088a10f4553bc
Charles Monjakidy <charlesmonjakidy@gmail.com>
2026-09-24 17:55:07 +0300

    Ajouter python-dotenv to requirements

--- backend/settings.py
+++ backend/settings.py
@@ -2,6 +2,9 @@ import os
 from pathlib import Path
 import dj_database_url
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 from dotenv import load_dotenv 
 load_dotenv(BASE_DIR / ".env")
