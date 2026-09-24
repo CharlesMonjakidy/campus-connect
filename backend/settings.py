6444159cae584e15eaf6f8e856c108b7862ddccb
Charles Monjakidy <charlesmonjakidy@gmail.com>
2026-09-24 18:21:06 +0300

    Fix Vercel build configuration

--- backend/settings.py
+++ backend/settings.py
@@ -89,7 +89,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 STATICFILES_DIRS = [BASE_DIR / "frontend" / "static",]
 
 MEDIA_URL = "/media/"
